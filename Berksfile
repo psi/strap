71c7395a7ef2d2d0fcf74c8e3ae78683f7a48c63
JD Harrington <jd@jdharrington.net>
2012-10-10 18:48:02 -0400

    add stuff to install ruby

--- Berksfile
+++ Berksfile
@@ -2,3 +2,5 @@ cookbook "xcode_command_line_tools",
   git: "https://github.com/psi/chef-xcode-command-line-tools.git"
 
 cookbook "homebrew"
+cookbook "ruby",
+  path: "~/src/cookbooks/ruby"
--- roles/workstation.rb
+++ roles/workstation.rb
@@ -1,3 +1,14 @@
 name "workstation"
 description "it's a workstation"
-run_list ["recipe[xcode_command_line_tools]", "recipe[homebrew]"]
+
+run_list [
+  "recipe[xcode_command_line_tools]",
+  "recipe[homebrew]",
+  "recipe[ruby]"
+]
+
+override_attributes(
+  "xquartz" => {
+    "url" => "http://xquartz.macosforge.org/downloads/SL/XQuartz-2.7.2.dmg"
+  }
+)
