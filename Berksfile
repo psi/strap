ccdbe635a3dde4464fe993a3ad7a82c7e60ae0a9
JD Harrington <jd@jdharrington.net>
2012-10-15 15:55:56 -0400

    switch to using fnichol's rvm cookbook

--- Berksfile
+++ Berksfile
@@ -2,5 +2,13 @@ cookbook "xcode_command_line_tools",
   git: "https://github.com/psi/chef-xcode-command-line-tools.git"
 
 cookbook "homebrew"
-cookbook "ruby",
-  path: "~/src/cookbooks/ruby"
+
+cookbook "gcc42",
+  path: "~/src/cookbooks/gcc42"
+
+cookbook "xquartz",
+  git: "git://github.com/jtimberman/xquartz-cookbook.git"
+
+cookbook "rvm",
+  path: "~/src/cookbooks/rvm"
+  #git: "git://github.com/fnichol/chef-rvm.git"
--- roles/workstation.rb
+++ roles/workstation.rb
@@ -2,12 +2,33 @@ name "workstation"
 description "it's a workstation"
 
 run_list [
+  # Stuff to build other stuff
   "recipe[xcode_command_line_tools]",
   "recipe[homebrew]",
-  "recipe[ruby]"
+  "recipe[gcc42]",
+  "recipe[xquartz]",
+
+  # Ruby! Ruby! Ruby!
+  "recipe[rvm::user]"
 ]
 
 override_attributes(
+  "build-essential" => {
+    "osx" => {
+      "gcc_installer_checksum" => "744fcc34be7da9206adc494d25ecd92bf9d8a4118588cc3e0464788b2a665493"
+    }
+  },
+
+  "rvm" => {
+    "user_installs" => [
+      {
+        "user"         => "jd",
+        "default_ruby" => "",
+        "rubies"       => ["1.9.3", "ree"]
+      }
+    ]
+  },
+
   "xquartz" => {
     "version" => "2.7.4",
     "url" => "http://xquartz.macosforge.org/downloads/SL/XQuartz-2.7.4.dmg",
